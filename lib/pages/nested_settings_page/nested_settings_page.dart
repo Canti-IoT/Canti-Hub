0359c26f6844b69df11b5411e600aac4d43cd558
Catalin Plesu <catalinhimself@gmail.com>
2024-03-16 19:17:15 +0200

    feat: add popup for wifi and mqtt

--- lib/pages/nested_settings_page/nested_settings_page.dart
+++ lib/pages/nested_settings_page/nested_settings_page.dart
@@ -52,13 +52,178 @@ class NestedSettingsPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AlarmConfigPage()),
-          );
+          switch (pageType) {
+            case SettingsType.recurrence:
+              break;
+            case SettingsType.wifi:
+              _showWifiPopup(context);
+              break;
+            case SettingsType.mqtt:
+              _showMqttPopup(context);
+              break;
+            case SettingsType.alarms:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AlarmConfigPage()),
+              );
+              break;
+            default:
+              break;
+          }
         },
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  Future<void> _showWifiPopup(BuildContext context) async {
+    String ssid = "Initial SSID";
+    String password = "Initial Password";
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("WiFi Connection"),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.text,
+                initialValue: ssid,
+                onChanged: (value) {
+                  // Update ssid when input changes
+                  ssid = value;
+                },
+                decoration: InputDecoration(labelText: "SSID"),
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                initialValue: password,
+                onChanged: (value) {
+                  // Update password when input changes
+                  password = value;
+                },
+                decoration: InputDecoration(labelText: "Password"),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showMqttPopup(BuildContext context) async {
+    String serverUrl = "Initial SSID";
+    String port = "Initial SSID";
+    String key = "api key";
+    String username = "username";
+    String password = "";
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("MQTT Connection"),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  initialValue: serverUrl,
+                  onChanged: (value) {
+                    // Update serverUrl when input changes
+                    serverUrl = value;
+                  },
+                  decoration: InputDecoration(labelText: "Server URL"),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  initialValue: port,
+                  onChanged: (value) {
+                    // Update port when input changes
+                    port = value;
+                  },
+                  decoration: InputDecoration(labelText: "Port"),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  initialValue: key,
+                  onChanged: (value) {
+                    // Update key when input changes
+                    key = value;
+                  },
+                  decoration: InputDecoration(labelText: "API Key"),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  initialValue: username,
+                  onChanged: (value) {
+                    // Update username when input changes
+                    username = value;
+                  },
+                  decoration: InputDecoration(labelText: "Username"),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  initialValue: password,
+                  onChanged: (value) {
+                    // Update password when input changes
+                    password = value;
+                  },
+                  decoration: InputDecoration(labelText: "Password"),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Perform actions with the MQTT connection settings here
+                // For example: connect to MQTT broker
+                print("Connecting to MQTT Broker...");
+                print("Server URL: $serverUrl");
+                print("Port: $port");
+                print("API Key: $key");
+                print("Username: $username");
+                print("Password: $password");
+
+                // Close the dialog
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
