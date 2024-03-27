c8475c2ce57c28aec397ca075e057fc4b3c9c65c
Catalin Plesu <catalinhimself@gmail.com>
2024-03-27 11:34:08 +0200

    fix: follow theme with icons

--- lib/pages/main_page/device_icon.dart
+++ lib/pages/main_page/device_icon.dart
@@ -9,54 +9,48 @@ class DeviceIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DetailPage()),
-          );
-        },
-        child: Container(
-          width: 76,
-          child: Stack(
-            children: [
-              // Image Container (below)
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(
-                    Color.fromRGBO(128, 128, 128, 0.2),
-                    BlendMode.srcOver,
-                  ),
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Image.asset(
-                      Files.bme680,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+    Color backgroundColor = Theme.of(context).backgroundColor;
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DetailPage()),
+        );
+      },
+      child: Container(
+        width: 76,
+        child: Stack(
+          children: [
+            // Image Container (below)
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Image.asset(
+                  Files.bme680,
+                  fit: BoxFit.cover,
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                right: 12,
-                child: Container(
-                  width: 18,
-                  height: 18,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.green,
-                    border: Border.all(color: Colors.white, width: 2),
-                  ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 12,
+              child: Container(
+                width: 18,
+                height: 18,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                  border: Border.all(color: backgroundColor, width: 2),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/main_page/plus_icon.dart
+++ lib/pages/main_page/plus_icon.dart
@@ -1,5 +1,8 @@
-import 'package:canti_hub/pages/detail_page/detail_page.dart';
+import 'package:canti_hub/database/custom_types.dart';
+import 'package:canti_hub/database/database.dart';
+import 'package:canti_hub/providers/database_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlusIcon extends StatelessWidget {
   const PlusIcon({
@@ -10,12 +13,7 @@ class PlusIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: GestureDetector(
-        onTap: () {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => DetailPage()),
-          // );
-        },
+        onTap: () async {},
         child: Container(
           width: 76,
           child: Stack(
@@ -23,22 +21,12 @@ class PlusIcon extends StatelessWidget {
               // Image Container (below)
               ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(
-                    Color.fromRGBO(128, 128, 128, 0.2),
-                    BlendMode.srcOver,
-                  ),
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[100],
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Icon(
-                      Icons.add_circle,
-                      size: 24.0,
-                    ),
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  child: Icon(
+                    Icons.add_circle,
+                    size: 36.0,
                   ),
                 ),
               ),
