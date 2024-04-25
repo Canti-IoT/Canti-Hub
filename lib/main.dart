224978c9b04d71babc587c84ee18109f0dc31600
Catalin Plesu <catalinhimself@gmail.com>
2024-04-25 21:19:03 +0300

    feat: add bluetooth library

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+import 'dart:async';
+
+import 'package:canti_hub/ble/ble_device_connector.dart';
+import 'package:canti_hub/ble/ble_device_interactor.dart';
+import 'package:canti_hub/ble/ble_logger.dart';
+import 'package:canti_hub/ble/ble_scanner.dart';
+import 'package:canti_hub/ble/ble_status_monitor.dart';
 import 'package:canti_hub/pages/start_page.dart';
 import 'package:canti_hub/providers/database_provider.dart';
 import 'package:canti_hub/providers/device_provider.dart';
@@ -6,26 +13,91 @@ import 'package:canti_hub/providers/settings_provider.dart';
 import 'package:canti_hub/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   requestAppPermissions();
-  runApp(const App());
+
+  final bleLogger = BleLogger();
+  final ble = FlutterReactiveBle();
+  final scanner = BleScanner(ble: ble, logMessage: bleLogger.addToLog);
+  final monitor = BleStatusMonitor(ble);
+  final connector =
+      BleDeviceConnector(ble: ble, logMessage: bleLogger.addToLog);
+  final serviceDiscoverer = BleDeviceInteractor(
+    // ignore: deprecated_member_use
+    bleDiscoverServices: ble.discoverServices,
+    readCharacteristic: ble.readCharacteristic,
+    writeWithResponse: ble.writeCharacteristicWithResponse,
+    writeWithOutResponse: ble.writeCharacteristicWithoutResponse,
+    subscribeToCharacteristic: ble.subscribeToCharacteristic,
+    logMessage: bleLogger.addToLog,
+  );
+
+  runApp(App(
+    bleLogger: bleLogger,
+    ble: ble,
+    scanner: scanner,
+    monitor: monitor,
+    connector: connector,
+    serviceDiscoverer: serviceDiscoverer,
+  ));
 }
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  final BleLogger bleLogger;
+  final FlutterReactiveBle ble;
+  final BleScanner scanner;
+  final BleStatusMonitor monitor;
+  final BleDeviceConnector connector;
+  final BleDeviceInteractor serviceDiscoverer;
+
+  const App({
+    Key? key,
+    required this.bleLogger,
+    required this.ble,
+    required this.scanner,
+    required this.monitor,
+    required this.connector,
+    required this.serviceDiscoverer,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => ParametersProvider()),
-        ChangeNotifierProvider(create: (context) => DatabaseProvider()),
-        ChangeNotifierProvider(create: (context) => SettingsProvider()),
-        ChangeNotifierProvider(create: (context) => ThemeProvider()),
-        ChangeNotifierProvider(create: (context) => DeviceProvider()),
+        Provider.value(value: scanner),
+        Provider.value(value: monitor),
+        Provider.value(value: connector),
+        Provider.value(value: serviceDiscoverer),
+        Provider.value(value: bleLogger),
+        StreamProvider<BleScannerState?>(
+          create: (_) => scanner.state,
+          initialData: const BleScannerState(
+            discoveredDevices: [],
+            scanIsInProgress: false,
+          ),
+        ),
+        StreamProvider<BleStatus?>(
+          create: (_) => monitor.state,
+          initialData: BleStatus.unknown,
+        ),
+        StreamProvider<ConnectionStateUpdate>(
+          create: (_) => connector.state,
+          initialData: const ConnectionStateUpdate(
+            deviceId: 'Unknown device',
+            connectionState: DeviceConnectionState.disconnected,
+            failure: null,
+          ),
+        ),
+        ChangeNotifierProvider(create: (_) => ParametersProvider()),
+        ChangeNotifierProvider(create: (_) => DatabaseProvider()),
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => DeviceProvider()),
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, child) {
@@ -45,12 +117,12 @@ class App extends StatelessWidget {
   }
 }
 
-// Function to request Bluetooth permissions
+// Function to request necessary permissions
 void requestAppPermissions() {
   Permission.bluetooth.request();
   Permission.bluetoothScan.request();
   Permission.bluetoothConnect.request();
   Permission.notification.request();
-  Permission.systemAlertWindow.request();
-  Permission.ignoreBatteryOptimizations.request();
+//   Permission.systemAlertWindow.request();
+//   Permission.ignoreBatteryOptimizations.request();
 }
--- lib/pages/main_page/main_page.dart
+++ lib/pages/main_page/main_page.dart
@@ -6,101 +6,112 @@ import 'package:canti_hub/pages/settings_page/settings_page.dart';
 import 'package:canti_hub/providers/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:provider/provider.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var localisation = AppLocalizations.of(context);
+  Widget build(BuildContext context) => Consumer<BleStatus?>(
+        builder: (_, status, __) {
+          if (status == BleStatus.ready) {}
+          print(status.toString());
+          var localisation = AppLocalizations.of(context);
 
-    return Scaffold(
-      appBar: CustomAppBar(
-          leftIcon: Icons.settings,
-          onLeftIconPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SettingsPage()),
-            );
-          },
-          rightIcon: Icons.notifications_none,
-          onRightIconPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AlarmsPage()),
-            );
-          }),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Horizontal scrollable list of avatars
-          Container(
-            margin: const EdgeInsets.only(left: 16, top: 16),
-            child: Text(
-              localisation!.devices,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const DevicesList(),
+          return Scaffold(
+            appBar: CustomAppBar(
+                leftIcon: Icons.settings,
+                onLeftIconPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingsPage()),
+                  );
+                },
+                rightIcon: Icons.notifications_none,
+                onRightIconPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AlarmsPage()),
+                  );
+                }),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Horizontal scrollable list of avatars
+                Container(
+                  margin: const EdgeInsets.only(left: 16, top: 16),
+                  child: Text(
+                    localisation!.devices,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const DevicesList(),
 
-          // Placeholder for the heatmap or any other content
-          Expanded(
-            child: Container(
-              child: GridView.count(
-                childAspectRatio:
-                    context.watch<SettingsProvider>().displayMode.index == 0
-                        ? 4
-                        : 1,
-                crossAxisCount:
-                    context.watch<SettingsProvider>().displayMode.index > 0
-                        ? context.watch<SettingsProvider>().displayMode.index
-                        : 1,
-                padding: EdgeInsets.all(16.0),
-                children: [
-                  // Add ParameterWidget instances for different parameters
-                  ParameterWidget(
-                    parameterName: 'Temperature',
-                    value: 26.0, // Replace with actual value
-                    desiredValue: 21.0,
-                    unit: '°C',
+                // Placeholder for the heatmap or any other content
+                Expanded(
+                  child: Container(
+                    child: GridView.count(
+                      childAspectRatio:
+                          context.watch<SettingsProvider>().displayMode.index ==
+                                  0
+                              ? 4
+                              : 1,
+                      crossAxisCount: context
+                                  .watch<SettingsProvider>()
+                                  .displayMode
+                                  .index >
+                              0
+                          ? context.watch<SettingsProvider>().displayMode.index
+                          : 1,
+                      padding: EdgeInsets.all(16.0),
+                      children: [
+                        // Add ParameterWidget instances for different parameters
+                        ParameterWidget(
+                          parameterName: 'Temperature',
+                          value: 26.0, // Replace with actual value
+                          desiredValue: 21.0,
+                          unit: '°C',
+                        ),
+                        ParameterWidget(
+                          parameterName: 'Humidity',
+                          value: 50.0, // Replace with actual value
+                          desiredValue: 50.0,
+                          unit: '%',
+                        ),
+                        ParameterWidget(
+                          parameterName: 'Air Quality(VOCs)',
+                          value: 0.6, // Replace with actual value
+                          desiredValue: 1.0,
+                          unit: '%',
+                        ),
+                        ParameterWidget(
+                          parameterName: 'Pressure',
+                          value: 1000.0, // Replace with actual value
+                          desiredValue: 1010.0,
+                          unit: 'Pa',
+                        ),
+                        ParameterWidget(
+                          parameterName: 'Noise Level',
+                          value: 30.0, // Replace with actual value
+                          desiredValue: 1.0,
+                          unit: 'dB',
+                        ),
+                        ParameterWidget(
+                          parameterName: 'Light Level',
+                          value: 500.0, // Replace with actual value
+                          desiredValue: 50.0,
+                          unit: 'lux',
+                        ),
+                      ],
+                    ),
                   ),
-                  ParameterWidget(
-                    parameterName: 'Humidity',
-                    value: 50.0, // Replace with actual value
-                    desiredValue: 50.0,
-                    unit: '%',
-                  ),
-                  ParameterWidget(
-                    parameterName: 'Air Quality(VOCs)',
-                    value: 0.6, // Replace with actual value
-                    desiredValue: 1.0,
-                    unit: '%',
-                  ),
-                  ParameterWidget(
-                    parameterName: 'Pressure',
-                    value: 1000.0, // Replace with actual value
-                    desiredValue: 1010.0,
-                    unit: 'Pa',
-                  ),
-                  ParameterWidget(
-                    parameterName: 'Noise Level',
-                    value: 30.0, // Replace with actual value
-                    desiredValue: 1.0,
-                    unit: 'dB',
-                  ),
-                  ParameterWidget(
-                    parameterName: 'Light Level',
-                    value: 500.0, // Replace with actual value
-                    desiredValue: 50.0,
-                    unit: 'lux',
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
+          );
+        },
+      );
 }
