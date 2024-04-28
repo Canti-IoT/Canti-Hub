8a4c1876ad2dc1a71d65806ecbae3caff97a44d3
Catalin Plesu <catalinhimself@gmail.com>
2024-04-28 17:39:52 +0300

    chore: create strings file

---
+++ lib/common/strings.dart
@@ -0,0 +1,14 @@
+class Strings {
+  static const String CONFIGURATION_SERVICE_UUID =
+      "cc944d76-a6b6-4a01-b7f8-77b02967f31f";
+  static const String CONFIG_CHARACTERISTIC_UUID =
+      "eda3d727-1b71-45d6-a07e-a0ea41fb0298";
+  static const String PARAMETER_INDEX_SERVICE_UUID =
+      "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+  static const String INDEX_CHARACTERISTIC_UUID =
+      "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+  static const String PARAMETER_VALUE_SERVICE_UUID =
+      "f35d596b-fff1-466d-97d8-ba175cd0a674";
+  static const String VALUE_CHARACTERISTIC_UUID =
+      "105158d4-fe10-4286-9a9f-4f3372788892";
+}
