00aeb8cb875678fdf0fb058068120282dd882535
Catalin Plesu <catalinhimself@gmail.com>
2024-05-05 13:30:06 +0300

    fix: new alarm with its number by default

--- lib/pages/settings_page/pages/alarms_page/alarms_page.dart
+++ lib/pages/settings_page/pages/alarms_page/alarms_page.dart
@@ -52,7 +52,7 @@ class AlarmsPage extends StatelessWidget {
           var alarm = await context
               .read<DatabaseProvider>()
               .insertAlarm(AlarmsTableCompanion.insert(
-                name: 'Alarm',
+                name: 'Alarm ${context.read<DatabaseProvider>().alarms.length + 1}',
                 activated: false,
               ));
 
